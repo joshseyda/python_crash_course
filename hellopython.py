97b96e03a9a4a96bea0912ba8041552ac82158fa
joshseyda <josh.seyda@gmail.com>
2018-07-10 15:18:38 -0400

    logical operators

--- hellopython.py
+++ hellopython.py
@@ -12,8 +12,6 @@ print('Hello Python!')
 name = "Josh"
 exclamation = "!"
 
-print("josh")
-
 # new names for data types .... List, Tuple, Map, Dictionary
 
 print("%s %s %s" % ("Hi my name is ", name, exclamation))
@@ -38,4 +36,17 @@ print(x)
 x=5
 x**=5
 print(x)
+
+#  Logical Operators
+
+truValue = True 
+falValue = False
+# both must be true
+print("X and Y", truValue and falValue)
+# either can be true
+print("X or Y", truValue or falValue)
+# returns the opposite of the variable
+print("not of X", not truValue)
+
+
 
