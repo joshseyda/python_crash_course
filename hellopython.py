777d17944c6134439e62e9c75b28ebfe70babcbd
joshseyda <josh.seyda@gmail.com>
2018-07-10 15:11:50 -0400

    assignment operators

--- hellopython.py
+++ hellopython.py
@@ -3,3 +3,39 @@ import sys
 import os
 
 print('Hello Python!')
+
+# single line 
+"""
+ multi line comment
+"""
+
+name = "Josh"
+exclamation = "!"
+
+print("josh")
+
+# new names for data types .... List, Tuple, Map, Dictionary
+
+print("%s %s %s" % ("Hi my name is ", name, exclamation))
+
+#  // is used to truncate the result of division ie 7/3 = 2
+
+# Assignment operators
+x=5
+print(x)
+
+x/=5
+print(x)
+
+x=5
+x%=5
+print(x)
+
+x=5
+x//=2
+print(x)
+
+x=5
+x**=5
+print(x)
+
