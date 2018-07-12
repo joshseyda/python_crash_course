f38feabf8dfca5ead2dc78d6012c89d8376e4b75
joshseyda <josh.seyda@gmail.com>
2018-07-12 12:50:13 -0400

    class variable accessible in instances

--- oop.py
+++ oop.py
@@ -2,34 +2,29 @@
 
 class Employee:
 
+  raise_amount = 1.04
+
   def __init__(self, first, last, pay):
     self.first = first
     self.last = last
     self.email = first + "." + last + "@email.com"
     self.pay = pay
-  def fullname(self):
+
+  def full_name(self):
     return '{} {}'.format(self.first, self.last)
+
+  def apply_raise(self):
+    self.pay = int(self.pay * self.raise_amount)  
 
 emp_1 = Employee("Josh","Sai",100000)
 emp_2 = Employee("Test","User",50000)
 
-"""
-emp_1.first = "Josh"
-emp_1.last = "Sai"
-emp_1.email = "josh.sai@email.com"
-emp_1.pay = 100000
-
-emp_2.first = "Test"
-emp_2.last = "User"
-emp_2.email = "test.user@email.com"
-emp_2.pay = 50000
-
+print(Employee.raise_amount)
+print(emp_1.full_name())
 print(emp_1.email)
-print(emp_2.email)
-
-"""
-print(emp_1.fullname())
-print(emp_1.email)
+print(emp_1.raise_amount)
 print("------")
-print(emp_2.fullname())
+print(emp_2.full_name())
 print(emp_2.email)
+print(emp_2.raise_amount)
+
