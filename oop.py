ead6e6e6a4d3665faaf0d6e79705a79fa43b5b84
joshseyda <josh.seyda@gmail.com>
2018-07-12 12:31:12 -0400

    manual class input

--- oop.py
+++ oop.py
@@ -1,5 +1,20 @@
 # Object Oriented Programming
 
-class Empoyee:
+class Employee:
   pass
 
+emp_1 = Employee()
+emp_2 = Employee()
+
+emp_1.first = "Josh"
+emp_1.last = "Sai"
+emp_1.email = "josh.sai@email.com"
+emp_1.pay = 100000
+
+emp_2.first = "Test"
+emp_2.last = "User"
+emp_2.email = "test.user@email.com"
+emp_2.pay = 50000
+
+print(emp_1.email)
+print(emp_2.email)
