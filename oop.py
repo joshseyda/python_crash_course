794426106a4acabe2eb01d6d53d0deaaaec85d75
joshseyda <josh.seyda@gmail.com>
2018-07-12 12:35:37 -0400

    constructor/initializer method

--- oop.py
+++ oop.py
@@ -1,11 +1,17 @@
 # Object Oriented Programming
 
 class Employee:
-  pass
 
-emp_1 = Employee()
-emp_2 = Employee()
+  def __init__(self, first, last, pay):
+    self.first = first
+    self.last = last
+    self.email = first + "." + last + "@email.com"
+    self.pay = pay
 
+emp_1 = Employee("Josh","Sai",100000)
+emp_2 = Employee("Test","User",50000)
+
+"""
 emp_1.first = "Josh"
 emp_1.last = "Sai"
 emp_1.email = "josh.sai@email.com"
@@ -15,6 +21,11 @@ emp_2.first = "Test"
 emp_2.last = "User"
 emp_2.email = "test.user@email.com"
 emp_2.pay = 50000
+
+print(emp_1.email)
+print(emp_2.email)
+
+"""
 
 print(emp_1.email)
 print(emp_2.email)
