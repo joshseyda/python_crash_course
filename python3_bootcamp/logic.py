7dc5bf89c3c8ddd0fb67690c096eaf637e181552
joshseyda <josh.seyda@gmail.com>
2018-08-03 17:11:17 -0400

    conditions

---
+++ python3_bootcamp/logic.py
@@ -0,0 +1,12 @@
+# conditional logic to make decisions!
+# if elif else
+name = input("What is your name?: ")
+if name == "Arya Stark":
+  print("Valar Morghulis")
+elif name == "Jon Snow":
+  print("You know nothing")
+elif name == "Daenerys Targaryen":
+  print("The Unburnt, The Breaker of Chains, The Mother of Dragons")
+else:
+  print("Carry on!")
+
